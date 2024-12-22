dfedeb5405ad59d0d0f43dd12cf2dda395e3830f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-23 04:20:20 +0800

    init layout

--- src/pages/users/new.vue
+++ src/pages/users/new.vue
@@ -2,16 +2,58 @@
   <div>
     <BaseBreadcrumb></BaseBreadcrumb>
 
-    <Stack>
+    <Stack :orientation="Orientation.Vertical">
       <header>
-        <h2>Create new user</h2>
+        <h1>Create new user</h1>
         <p>Create a new user in system.</p>
       </header>
+
+      <Stack :orientation="Orientation.Vertical">
+        <Stack>
+          <Label>Email</Label>
+          <TextField
+            :text-field-type="TextFieldType.Email"
+            v-model="email"
+          ></TextField>
+        </Stack>
+
+        <Stack>
+          <Label>Name</Label>
+          <TextField
+            :text-field-type="TextFieldType.Text"
+            v-model="name"
+          ></TextField>
+        </Stack>
+
+        <Stack>
+          <Label>Password</Label>
+          <Stack :orientation="Orientation.Horizontal">
+            <TextField
+              :text-field-type="TextFieldType.Password"
+              v-model="password"
+            ></TextField>
+            <Checkbox></Checkbox>
+          </Stack>
+        </Stack>
+
+        <Stack>
+          <Button :appearance="Appearance.Accent">Create</Button>
+        </Stack>
+      </Stack>
     </Stack>
+
+    <div>
+      <div>Email value: {{ email }}</div>
+      <div>Name value: {{ name }}</div>
+      <div>Password value: {{ password }}</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { TextFieldType } from "~/components/Base/TextField/types";
+import { Appearance, Orientation } from "~/components/Base/types";
+
 useHead({
   title: "New user",
 });
