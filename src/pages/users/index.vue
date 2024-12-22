bf142289b2cd460a1a17b69196e62be70a5836b8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-22 12:48:39 +0800

    Fluent button

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
-    <div>0 users found.</div>
+    <div style="display: flex">
+      <BaseButton :appearance="Appearance.Accent">New user</BaseButton>
+      <BaseButton :appearance="Appearance.Accent">Download users</BaseButton>
+      <BaseButton :appearance="Appearance.Accent">Bulk operations</BaseButton>
+      <BaseButton :appearance="Appearance.Accent">Refresh</BaseButton>
+      <BaseButton :appearance="Appearance.Accent">Manage view</BaseButton>
+    </div>
+
+    <div>{{ users.length }} user{{ users.length > 1 ? "s" : "" }} found.</div>
+
     <BaseDataGrid> </BaseDataGrid>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Appearance } from "~/components/Base/types";
+import type { IUserService, UserResponse } from "~/resources/users";
+
+const userService = useNuxtApp().$userService as IUserService;
+
+useHead({
+  title: "Users",
+});
+
+const users = ref<UserResponse[]>([]);
+
+onMounted(async () => {
+  const response = await userService.listUsers();
+  users.value = response;
+});
+</script>
 
 <style></style>
