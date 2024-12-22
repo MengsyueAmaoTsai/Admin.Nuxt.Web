df7616bc655128165e10284bf86e075eb39b9314
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-22 13:00:15 +0800

    Add /users/new

---
+++ src/pages/users/new.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Create new user</h2>
+    <p>Create a new user in system</p>
+
+    <!-- Tabs -->
+
+    <hr />
+
+    <div style="display: flex">
+      <BaseButton
+        :appearance="Appearance.Accent"
+        :onClick="() => console.log('Create user')"
+        >Review + create</BaseButton
+      >
+      <BaseButton
+        :appearance="Appearance.Outline"
+        :onClick="() => console.log('Previous step')"
+        >< Previous></BaseButton
+      >
+      <BaseButton
+        :appearance="Appearance.Outline"
+        :onClick="() => console.log('New step')"
+        >Next: Properties ></BaseButton
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Appearance } from "~/components/Base/types";
+
+useHead({
+  title: "New user",
+});
+</script>
+
+<style scoped lang="scss"></style>
