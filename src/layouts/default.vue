c8b56eb4e21534d40402c7f7661f9f10db78c201
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-23 10:13:56 +0800

    Add header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <FluLayout>
-      <slot />
+      <FluHeader>Richill Capital Admin</FluHeader>
+
+      <FluStack
+        class="main"
+        :orientation="Orientation.Horizontal"
+        :width="'100%'"
+      >
+        <slot />
+      </FluStack>
     </FluLayout>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Orientation } from "~/components/Flu/types";
+
 useHead({
   titleTemplate: (chunk) =>
     chunk ? `${chunk} - Richill Capital Admin` : "Richill Capital Admin",
